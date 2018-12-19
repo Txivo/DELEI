2bfb870414df761b9c2bd4b302b8e69ed8dd2b86
Txivo Juarez Lap <txivojuarezlap@MacBook-Pro-de-Txivo.local>
2018-12-19 16:13:09 -0600

    created contact, fpassword, testimonios.html

---
+++ conatct.php
@@ -0,0 +1,13 @@
+<?php 
+
+if (isset($_POST['submit'])){
+
+    $nombre = $_POST ['nombre'];
+    $telefono = $_POST ['telefono'];
+    $mail = $_POST ['mail'];
+    $mensaje = $_POST ['mensaje'];
+
+
+    $mailTo = "info@delei.org.mx";
+    
+}
